e60be599c55451c73bc28ebd2efe078e43847034
meganlambin <meg.lambin@hotmail.fr>
2015-12-22 17:39:56 +0100

    nouveau fichier (JS)

---
+++ script.js
@@ -0,0 +1,42 @@
+(function($) {
+  $.simplePlaceholder = {
+    placeholderClass: null,
+
+    hidePlaceholder: function(){
+      var $this = $(this);
+      if($this.val() == $this.attr('placeholder') && $this.data($.simplePlaceholder.placeholderData)){
+        $this
+          .val("")
+          .removeClass($.simplePlaceholder.placeholderClass)
+          .data($.simplePlaceholder.placeholderData, false);
+      }
+    },
+
+    showPlaceholder: function(){
+      var $this = $(this);
+      if($this.val() == ""){
+        $this
+          .val($this.attr('placeholder'))
+          .addClass($.simplePlaceholder.placeholderClass)
+          .data($.simplePlaceholder.placeholderData, true);
+      }
+    },
+
+    preventPlaceholderSubmit: function(){
+      $(this).find(".simple-placeholder").each(function(e){
+        var $this = $(this);
+        if($this.val() == $this.attr('placeholder') && $this.data($.simplePlaceholder.placeholderData)){
+          $this.val('');
+        }
+      });
+      return true;
+    }
+  };
+
+  $.fn.simplePlaceholder = function(options) {
+    if(document.createElement('input').placeholder == undefined){
+      var config = {
+        placeholderClass : 'placeholding',
+        placeholderData : 'simplePlaceholder.placeholding'
+      };
+
